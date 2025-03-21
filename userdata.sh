cd5c2795c5c5f2a15bb963e6272947cd5b17477f
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-21 22:08:55 +0530

    changes

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set-hostname rabbitmq
-yum install ansibe -y &>>/opt/userdata.log
+yum install ansible -y &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/Revanthsatyam/roboshop-ansible-d76.git -e component=rabbitmq main.yml &>>/opt/userdata.log
